596cd9a2a087b52115bca4758298aad04ffff111
unknown <abhitripathi2312gmail.com>
2020-11-29 21:40:03 +0530

    material ui to design the button and input field

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 const Todo = ({ todo }) => (
-	<div>
-		<li>{todo}</li>
-	</div>
+	<List>
+		<ListItem>
+			<ListItemText primary={todo} secondary="Dummy deadline" />
+		</ListItem>
+	</List>
 );
 
 export default Todo;
